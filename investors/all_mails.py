eb4cd5a998c801d45322cd640d3ebd45fdf71f67
kchaitanya954 <kchaitanya954@gmail.com>
2024-01-29 17:12:23 +0000

    collected 172590 mains

--- investors/all_mails.py
+++ investors/all_mails.py
@@ -115,7 +115,7 @@ def main():
     companies = ['apple', 'microsoft', 'saudi aramco', 'alphabet', 'google', 'amazon', 'nvidia', 'meta platforms', 'facebook', 'berkshire hathaway ', 'tesla', 'tsmc', 'eli lilly', 'broadcom', 'visa', 'jpmorgan chase', 'unitedhealth', 'novo nordisk', 'walmart', 'mastercard', 'exxon mobil', 'johnson & johnson', 'lvmh', 'samsung', 'procter & gamble', 'home depot', 'tencent', 'asml', 'oracle', 'costco', 'merck', 'nestlé', 'abbvie', 'amd', 'kweichow moutai', 'adobe', 'toyota', 'chevron', 'salesforce', 'bank of america ', 'coca-cola', "l'oréal", 'netflix', 'international holding company', 'accenture', 'icbc', 'roche', 'pepsico', 'novartis', 'reliance industries', 'mcdonald', 'hermès', 'thermo fisher scientific', 'intel', 'cisco', 'astrazeneca', 'sap', 'shell', 'linde', 'abbott laboratories', 'pinduoduo', 't-mobile us', 'alibaba', 'china mobile', 'wells fargo', 'intuit', 'agricultural bank of china', 'petrochina', 'comcast', 'verizon', 'qualcomm', 'walt disney', 'danaher', 'tata consultancy services', 'amgen', 'pfizer', 'servicenow', 'ibm', 'bhp group', 'texas instruments', 'nike', 'bank of china', 'totalenergies', 'hsbc', 'union pacific corporation', 'caterpillar', 'china construction bank', 'applied materials', 'siemens', 'morgan stanley', 's&p global', 'philip morris', 'general electric', 'hdfc bank', 'royal bank of canada', 'american express', 'united parcel service', 'honeywell', 'prosus', 'inditex', 'uber', 'conocophillips']
     for company in companies[3:]:
         if company == 'alphabet':
-            json_names = json_names[860:]
+            json_names = json_names[1480:]
         for obj in json_names:
             count += 1
             if count%10==0:
