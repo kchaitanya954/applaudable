c22b89e425204c74c1096c6a88a4644dc2dc3d1f
kchaitanya954 <kchaitanya954@gmail.com>
2024-05-02 16:43:55 +0000

    8th school

--- schools/main.py
+++ schools/main.py
@@ -146,11 +146,11 @@ def main():
                 "The Olive Tree School",
                 "Zürich Schule Barcelona"
             ]
-    for school in schools[4:]:
+    for school in schools[7:]:
         count = 0
         print(school)
-        if school == "Agora Sant Cugat International School":
-            json_names = json_names[377:]
+        if school == "École Française Ferdinand de Lesseps":
+            json_names = json_names[40:]
         school = school.lower()
         for obj in json_names:
             count += 1
