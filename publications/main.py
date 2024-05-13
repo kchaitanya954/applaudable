bc25b7764f4c2b3e868f2ba722414b8100d77c78
kchaitanya954 <kchaitanya954@gmail.com>
2024-05-13 13:54:28 +0000

    mails for verge

--- publications/main.py
+++ publications/main.py
@@ -100,7 +100,7 @@ def stop_vpn():
 
 def main():
     json_names = []
-    with open('pub_names/wired_names.csv', 'r') as file:
+    with open('pub_names/theverge_names.csv', 'r') as file:
         # Read each line as a separate JSON object
         reader = csv.DictReader(file)
         for row in reader:
@@ -126,7 +126,7 @@ def main():
         
         obj['email_addresses'] = email_addresses
         obj['mobile_numbers'] = mobile_numbers
-        save_to_csv(obj, 'data/mails_wired.csv')
+        save_to_csv(obj, 'data/mails_theverge.csv')
         time.sleep(0.2)
         
 if __name__ == "__main__":
